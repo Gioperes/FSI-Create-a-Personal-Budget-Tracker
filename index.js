11eab24da93341feee72e2aef32bc7d4d1953f48
Gioperes <103082092+Gioperes@users.noreply.github.com>
2022-04-21 19:33:32 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 //window.prompt(weeklyExpenseQuestions[0])
-for(){
-let answer = parseFloat(window.prompt(weeklyExpenseQuestions[0]))
+for(let i= 0; i < weeklyExpenseQuestions.length;i++){
+let answer = parseFloat(window.prompt(weeklyExpenseQuestions[i]))
 weeklyExpenses = weeklyExpenses + answer}
